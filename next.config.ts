40b9534dcc9bb658a1f1bf87f9ebabff7b1921b1
Sina <sina@example.com>
2026-03-06 14:18:15 +0100

    chore: setup github pages static export workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/OopsCupid",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
