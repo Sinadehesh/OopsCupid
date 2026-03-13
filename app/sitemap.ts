d632366bd507d4ea644313d530046fd963afe78b
Sina Dehesh <112756363+Sinadehesh@users.noreply.github.com>
2026-03-13 18:17:10 +0000

    feat(seo): add dynamic sitemap generation

---
+++ app/sitemap.ts
@@ -0,0 +1,45 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://oopscupid.com'
+
+  // List of all your important routes
+  const routes = [
+    '',
+    '/quizzes',
+    '/him',
+    '/me',
+    '/friends',
+    '/articles',
+    // Dating & Him Tools
+    '/is-he-manipulative',
+    '/is-he-gaslighting-me',
+    '/is-he-cheating',
+    '/dating-texting-analysis',
+    '/partners-attachment-style',
+    // Me & Self Reflection
+    '/attachment-style-quiz',
+    '/why-do-i-attract-toxic-people',
+    '/attraction-patterns',
+    '/why-do-i-sabotage-relationships',
+    // Friendships
+    '/friend-group-role',
+    '/toxic-friend-test',
+    '/are-my-friends-bad-for-me',
+    '/are-your-friends-using-you',
+    // Articles
+    '/articles/manipulation-science',
+    '/articles/is-my-friend-toxic',
+    '/love-bombing-signs',
+    '/trauma-bonding-signs',
+    '/understanding-attachment-styles',
+    '/signs-of-a-toxic-friend'
+  ]
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: route === '' ? 1 : 0.8, // Homepage is highest priority
+  }))
+}
